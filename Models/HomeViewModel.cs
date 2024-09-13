dbed7dfcdf219a382a49acf17ef098cfdd5f3bd3
DaniilKylik <daniel.kulyk2004@gmail.com>
2024-09-13 20:08:33 +0300

    Lr2.Net
    Lr2.Net

---
+++ Models/HomeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Lr1.Net.Models
+{
+    public class HomeViewModel
+    {
+        public string LargestCompany { get; set; }
+        public PersonalInfo PersonalInfo { get; set; }
+    }
+}
