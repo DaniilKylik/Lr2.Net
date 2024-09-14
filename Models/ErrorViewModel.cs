1d4f2dc6fd32519784b0589049b48e5f6ba8c633
DaniilKylik <daniel.kulyk2004@gmail.com>
2024-09-14 11:28:05 +0300

    Lr2.Net
    Lr2.Net

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Lr1.Net.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Models/HomeViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Lr1.Net.Models
-{
-    public class HomeViewModel
-    {
-        public string LargestCompany { get; set; }
-        public PersonalInfo PersonalInfo { get; set; }
-    }
-}
